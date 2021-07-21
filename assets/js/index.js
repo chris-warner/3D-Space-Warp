448c0ec15ae14498af82b7a2c51e8ed4ff65f491
Christopher Warner <cwarner@concentrichx.com>
2021-07-21 09:57:59 -0400

    Update to V.1.

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,6 +19,8 @@ function init() {
            Math.random() * 600 - 300,
            Math.random() * 600 - 300
        ); 
+       star.velocity = 0;
+       star.acceleration = 0.02;
        starGeo.vertices.push(star);
     }
        let sprite = new THREE.TextureLoader().load('assets/images/star.png');
@@ -33,7 +35,25 @@ function init() {
     animate();
 }
 
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
 function animate() {
+    starGeo.vertices.forEach(p => { 
+        p.velocity += p.acceleration;
+        p.opacity = Math.random();
+        p.y -= p.velocity;
+        if(p.y <-200) {
+            p.y = 200;
+            p.velocity = 0;
+        }
+    });
+    starGeo.verticesNeedUpdate = true;
+    stars.rotation.y += 0.002;
+    stars.opacity = Math.random()/2;
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
 }
